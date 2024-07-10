07c2161b275903bd1c6f9814551de723155bfbf6
Felipe Matos <felipematosapps@gmail.com>
2024-07-10 15:34:32 -0300

    finalizado tela de interface do app

--- app/src/main/java/com/calculadoraapp/MainActivity.kt
+++ app/src/main/java/com/calculadoraapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.calculadoraapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,4 +13,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+
 }
